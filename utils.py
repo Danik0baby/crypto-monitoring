df10ae22c06be837830656b8f227aa949d44cb74
Danik0baby <app.store01.10.80@gmail.com>
2026-08-07 23:21:46 +0500

    asynchrony in send telegram message

--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
 import requests
 import logging
+import aiohttp
+import asyncio
 from pybit.unified_trading import HTTP
 from config import (
     TELEGRAM_TOKEN,
@@ -10,16 +12,17 @@ from config import (
 
 session = HTTP(testnet=False)
 
-def send_telegram_message(text):
+async def send_telegram_message(text):
     url = "https://api.telegram.org/bot{}/sendMessage".format(TELEGRAM_TOKEN)
     payload = {"chat_id": CHAT_ID, "text": text}
     try:
-        response = requests.post(url, json=payload)
-        if response.status_code != 200:
-            logging.error("Ошибка отправки в Telegram: {}".format(response.text))
+        async with aiohttp.ClientSession() as client:
+            async with client.post(url, json=payload) as response:
+                if response.status_code != 200:
+                    error = await response.text()
+                    logging.error("Ошибка отправки в Telegram: {}".format(response.text))
     except Exception as err:
         logging.error("Ошибка сети при отправке в Telegram: {}".format(err))
-        
         
         
 def get_current_price():
